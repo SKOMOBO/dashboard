5eefbfe1bc187965fd6bd27888c4df4745318c45
Ulfasaar <weyers.ryan@gmail.com>
2017-11-22 21:28:30 +1300

    Whole thing works including server interaction

--- src/App.js
+++ src/App.js
@@ -6,13 +6,23 @@ import axios from 'axios';
 
 class App extends Component {
   // just run react app on development server it will call express already
-  render() {
-    axios.get("http://localhost/who-am-i").then(res=>{
-      console.log(res)
-    }).catch(err=>{
-      console.log(err)
+  constructor(props){
+    super(props);
+    this.state={name:"no one"};
+  }
+ 
+  componentWillMount(){
+    axios.get("http://localhost:81/who-am-i").then(resp=>{
+      console.log(resp.data)
+      let name = resp.data.first_name + " " + resp.data.last_name;
+      this.setState({name});
+    }).catch(error=>{
+      console.log(error)
     })
-    // console.log(axios)
+  }
+  
+  render() {
+
     return (
       <div className="App">
         <header className="App-header">
@@ -22,7 +32,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
 
-          HELLO WORLD!!!
+          HELLO {this.state.name}!!!
         </p>
       </div>
     );
