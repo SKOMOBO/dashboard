8ddc26c6e6aa59a64882a8e2e7e9364d031fc1a3
Ulfasaar <weyers.ryan@gmail.com>
2017-11-26 22:33:45 +1300

    Made a basic chart using recharts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
-import mapbox from 'mapbox-gl/dist/mapbox-gl.js';
 import logo from './logo.svg';
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import ReactMapboxGl from "react-mapbox-gl";
 import './App.css';
 
 // import ReactDOM from 'react-dom';
 
 // import axios from 'axios';
-// import './react-d3-library'
+
+import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from "recharts"
+class SimpleLineChart extends Component{
+
+  render(){
+    let data = [
+      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+    ];
+
+    return (
+      <LineChart width={600} height={300} data={data}
+      margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+      <XAxis dataKey="name"/>
+      <YAxis/>
+      <CartesianGrid strokeDasharray="3 3"/>
+      <Tooltip/>
+      <Legend />
+      <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
+      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      </LineChart>
+    );
+  }
+}
 
 class App extends Component {
   // just run react app on development server it will call express already
@@ -44,8 +71,8 @@ class App extends Component {
       doubleClickZoom : false,
       dragPan : false
     });
-
     return ( 
+
       <div className="App">
         {/*<link href='https://api.mapbox.com/mapbox-gl-js/v0.42.0/mapbox-gl.css' rel='stylesheet' /> */}
         <header className="App-header">
@@ -78,6 +105,7 @@ class App extends Component {
           </Layer>*/}
       </Map>
       </div>
+      <SimpleLineChart></SimpleLineChart>
         {/*<div id="map" style={{width:"2000px", height:"500px"}}></div>*/}
         {/* this is a example plotly chart that only gets shown once its finished loading its very slow*/}
         {/*<iframe style={{display: this.state.show_chart}} onLoad ={()=>{this.setState({show_chart:"inline"})}} title="test_chart" src="https://plot.ly/~tsusnjak/12" width="1000px" height="400px"></iframe>*/}
