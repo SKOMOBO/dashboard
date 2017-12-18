5d7ad2442dc7676a39bf668a429910e3a5d4f10e
Ulfasaar <weyers.ryan@gmail.com>
2017-12-18 19:18:27 +1300

    Got basic hot code reloading working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-// import ReactMapboxGl from "react-mapbox-gl";
 import './App.css';
 
 // import ReactDOM from 'react-dom';
@@ -12,7 +11,7 @@ class SimpleLineChart extends Component{
 
   render(){
     let data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+      {name: 'Page A', uv: 4000, pv: 2400, amt: 2410},
       {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
       {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
       {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
@@ -46,14 +45,9 @@ class App extends Component {
  
   
   componentDidMount(){
-    
-    // var map = new mapbox.Map({
-    // container: 'map',
-    // style: 'mapbox://styles/mapbox/dark-v9'
-    // });
   }
   // componentWillMount(){
-    // ! use this sort of function to pull data from the server, store it in global variables
+    // ! use this sort of function to pull data from the server, store it in global variables caches?
 
     // axios.get("http://localhost:81/who-am-i").then(resp=>{
     //   console.log(resp.data)
@@ -65,19 +59,13 @@ class App extends Component {
   // }
 
   render() {
-    // const Map = ReactMapboxGl({
-    //   accessToken : 'pk.eyJ1IjoibWFzc2V5LWRlc2lnbmVyIiwiYSI6ImNqYWRyd2lpbzBtcm4ycXBsaWI0OHRxbzcifQ.tJb630r3fR883YGRpCYYJg',
-    //   touchZoomRotate : false,
-    //   doubleClickZoom : false,
-    //   dragPan : false
-    // });
+
     return ( 
 
       <div className="App">
-        {/*<link href='https://api.mapbox.com/mapbox-gl-js/v0.42.0/mapbox-gl.css' rel='stylesheet' /> */}
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to SKOMOBO</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -85,30 +73,10 @@ class App extends Component {
           HELLO {this.state.name}!!!
         </p>
 
-       {/* <div style={{paddingLeft:"35vw"}}>
-        <Map
-        // style="mapbox://styles/mapbox/streets-v9"
-        style="mapbox://styles/mapbox/dark-v9"
-        center ={[173.19451905919522,-40.879852995838554]}
-        zoom={[4]}
-        containerStyle={{
-          height: "60vh",
-          width: "30vw"
-        }}>*/}
-        
-         {/* <Layer
-            type="symbol"
-            id="marker"
-            layout={{ "icon-image": "marker-15" }}>
-            <Feature coordinates={[40.9006, 174.8860]}/>
-            {/*<Feature coordinates={[-0.481747846041145, 51.3233379650232]/>
-          </Layer>
-      </Map>
-      </div>*/}
-      <SimpleLineChart></SimpleLineChart>
-        {/*<div id="map" style={{width:"2000px", height:"500px"}}></div>*/}
-        {/* this is a example plotly chart that only gets shown once its finished loading its very slow*/}
-        {/*<iframe style={{display: this.state.show_chart}} onLoad ={()=>{this.setState({show_chart:"inline"})}} title="test_chart" src="https://plot.ly/~tsusnjak/12" width="1000px" height="400px"></iframe>*/}
+        <div style={{paddingLeft:"30vw"}}>
+          <SimpleLineChart></SimpleLineChart>
+        </div>
+       
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,52 +4,25 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// import { Chart } from "react-d3-core";
-// import { LineChart } from "react-d3-basic";
+// hot code reloading based on https://medium.com/superhighfives/hot-reloading-create-react-app-73297a00dcad
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root')
 
-// var width = 700,
-// height = 300,
-// margins = {left: 100, right: 100, top: 50, bottom: 50},
-// title = "User sample",
-// // chart series,
-// // field: is what field your data want to be selected
-// // name: the name of the field that display in legend
-// // color: what color is the line
-// chartData = [
-//   // put your data here!
-//   1, 2, 3 ,4 ,5
-// ],
-// chartSeries = [
-//   {
-//     field: 'BMI',
-//     name: 'BMI',
-//     color: '#ff7f0e'
-//   }
-// ],
-// // your x accessor
-// x = function(d) {
-//   return d.index;
-// }
+ReactDOM.render(<App />, rootEl);
 
-// ReactDOM.render( <Chart
-//     title={title}
-//     width={width}
-//     height={height}
-//     margins= {margins}
-//     >
-//     <LineChart
-//       margins= {margins}
-//       title={title}
-//       data={chartData}
-//       width={width}
-//       height={height}
-//       chartSeries={chartSeries}
-//       x={x}
-//     />
-//   </Chart>, document.getElementById('line-user'))
+if (module.hot) {
+    module.hot.accept('./App', () => {
+      const NextApp = require('./App').default
+      ReactDOM.render(
+        <NextApp/>,
+        rootEl
+      )
+    })
+  }
+
 registerServiceWorker();
+
+
 
 // todo setup websocket connection with server, pulls all the stuff from db 
 // via websocket connection
